f4db6aa12591935f88438e95d6692b445bf5687f
Renan Gandra Goes <147532325+renangandra@users.noreply.github.com>
2023-12-11 19:41:45 +0100

    'useful mock data for testing'

---
+++ src/app/utils/mockedValues.ts
@@ -0,0 +1,11 @@
+import { PropsTask } from '../interfaces/tasks';
+
+export const mockedTasks: PropsTask[] = [
+  { id: 1, title: 'Aprender React', completed: true },
+  { id: 2, title: 'Estudar NodeJS', completed: false },
+];
+
+export const mockedTasksSeeders: PropsTask[] = [
+  { title: 'Aprender React',  },
+  { title: 'Estudar NodeJS',  },
+];
